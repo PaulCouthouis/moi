31e3d3747bf4e6d5700508dd25e2634d566ae065
Paul Couthouis <couthouis.p@baseconnect.in>
2021-08-22 13:05:42 +0900

    design : max-width for method block

--- packages/client/components/MethodsInformation.tsx
+++ packages/client/components/MethodsInformation.tsx
@@ -5,13 +5,13 @@ export const MethodsInformation: React.FC<{
   methodsInformation: MethodInformationEntity[]
 }> = ({ methodsInformation }) => {
   const articleItems = methodsInformation.map(({ title, icon, description }, i) => (
-    <x.article key={i} w={{ _: '100%', md: 'calc(50% - 10px)' }} m='10px 10px 10px 0' display='flex'>
+    <x.article key={i} maxWidth={{ md: 'calc(50% - 10px)' }} m='10px 10px 10px 0' display='flex'>
       <x.div marginRight={25}>
         <x.span className={`lnr lnr-${icon}`} fontSize={40} color='#b5b5b5' />
       </x.div>
       <x.div>
         <x.h3 fontSize={{ _: 14, xxs: 18 }} fontWeight={600} lineHeight='40px'>{title}</x.h3>
-        <x.p fontSize={12} lineHeight='1.85em'>{description}</x.p>
+        <x.p fontSize={12} lineHeight='1.85em' maxWidth='65ch'>{description}</x.p>
       </x.div>
     </x.article>
   ))
@@ -19,7 +19,7 @@ export const MethodsInformation: React.FC<{
   return (
     <x.section bg='#222' p='40px 30px 60px'>
       <x.h2 display='inline-block' marginBottom='20px' paddingBottom='3px' fontSize={21} fontWeight={600} borderBottom='2px solid #0099e5'>Ma Méthodologie</x.h2>
-      <x.div display='flex' flexWrap='wrap'>
+      <x.div display='flex' flexWrap='wrap' justifyContent='space-around'>
         {articleItems}
       </x.div>
     </x.section>
